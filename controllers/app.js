2dae6a69735c32d266370779140b1f02c640e280
Thalles <thalles753@gmail.com>
2017-06-07 15:13:44 -0300

    removing

--- controllers/app.js
+++
@@ -1,7 +0,0 @@
-angular.module("MyBlog", [], function($interpolateProvider) {
-  $interpolateProvider.startSymbol('[[');
-  $interpolateProvider.endSymbol(']]');
-})
-  .run(function () {
-    console.log("App ready!")
-  })
